99848f369d68fe860a36b247f8d0b951aa44d2ce
Michele Debris-Hue <debrishue.michele@gmail.com>
2018-09-03 10:13:37 +0100

    Add images

--- src/javascript/component/projectContainerSmall/ProjectContainerSmall.js
+++ src/javascript/component/projectContainerSmall/ProjectContainerSmall.js
@@ -4,7 +4,6 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types';
 import styles from '../../../styles/projectContainerSmall/projectContainerSmall.scss';
-import placeholder150 from '../../../images/150.jpg';
 
 class ProjectContainerSmall extends Component {
     /*static propTypes = {
@@ -18,7 +17,7 @@ class ProjectContainerSmall extends Component {
         return (
           <Link to={`/${this.props.project.id}`} className={styles.linkToProjectPage}>
             <div className={styles.project}>
-                <img src={placeholder150} alt={this.props.project.title} className={styles.projectImg} />
+                <img src={this.props.project.image} alt={this.props.project.title} className={styles.projectImg} />
                 <h2 className={styles.projectTitle}>{this.props.project.title}</h2>
                 <p className={styles.projectDescription}>{this.props.project.description}</p>
             </div>
--- src/javascript/constant/ProjectsData.js
+++ src/javascript/constant/ProjectsData.js
@@ -1,8 +1,13 @@
+import derbyshireGroupMain from '../../images/derbyshireGroup.png';
+import dansMaCuisineMain from '../../images/dansMaCuisineMain.png';
+import arcadeGame from '../../images/arcadeGame.png';
+
 const projectsData = new Map();
 
 const derbyshireGroup = {
     id: 'derbyshire-group',
     title: 'Derbyshire Group',
+    image: derbyshireGroupMain,
     description:
         'Garage responsive website, with one page per service, and a contact page including a contact form and Google Map. Done in only a couple months while working full time, everything was created from scratch (design, content, features...).',
     descriptionDetailed:
@@ -29,6 +34,7 @@ projectsData.set(derbyshireGroup.id, derbyshireGroup);
 const dansMaCuisine = {
     id: 'dans-ma-cuisine',
     title: 'Dans ma cuisine',
+    image: dansMaCuisineMain,
     description:
         'Personal project to train on latest technologies (React, Mobx, Intl, Lunr...). It is a recipes responsive website, where recipes are sorted by type, but you can also search for a particular recipe or ingredient. The whole website can be translated in French or English.',
     descriptionDetailed:
@@ -42,6 +48,7 @@ projectsData.set(dansMaCuisine.id, dansMaCuisine);
 const smallProjects = {
     id: 'small-projects',
     title: 'Old small projects (blog-redux, Nutella, arcade game)',
+    image: arcadeGame,
     description:
         "Small various projects, done either to train or as interview exercises. Most of them are from the beginning of my training, so not really 'code-wow'. But still, I was proud of them at the time, so they have their place here.",
     descriptionDetailed:
