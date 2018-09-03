21729a34b3d53c965c5c4517abe38640cb8c6ef5
Michele Debris-Hue <debrishue.michele@gmail.com>
2018-09-03 09:35:38 +0100

    Add contact page

--- src/javascript/component/contact/Contact.js
+++ src/javascript/component/contact/Contact.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
+import styles from '../../../styles/contact/contact.scss';
+
 export default function Contact() {
     return (
-        <div>
-            <p>I am the Contact page</p>
+        <div className={styles.contactBody}>
+            <h1 className={styles.contactTitle}>Contact me</h1>
+            <h3 className={styles.name}>Michele Debris-Hue</h3>
+            <p className={styles.email}>debrishue.michele@gmail.com</p>
+            <a
+                href="https://www.linkedin.com/in/mich%C3%A8le-debris-hue-1335b0152/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.link}
+            >
+                LinkedIn
+            </a>
+            <a href="https://github.com/micheledh" target="_blank" rel="noopener noreferrer" className={styles.link}>
+                Github
+            </a>
+            <p className={styles.quote}>Free quote on demand !</p>
         </div>
     );
 }
